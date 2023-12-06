653202be87b18ad54578466d16c2f8b4787542a2
aamfahim <97575047+aamfahim@users.noreply.github.com>
2023-12-06 11:08:57 -0500

    added Update and Delete component

--- src/Components/fragment.js
+++ src/Components/fragment.js
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 import GetAll from './GetAll';
 import PostForm from './PostForm';
 import GetById from './GetById';
+import Update from './Update';
+import Delete from './Delete';
 
 export default function Fragment() {
     const [activeComponent, setActiveComponent] = useState('GetAll');
@@ -27,10 +29,24 @@ export default function Fragment() {
                 >
                     Post
                 </button>
+                <button
+                    onClick={() => setActiveComponent('Update')}
+                    className="mt-5 rounded-full justify-end bg-blue-600 hover:bg-blue-700 py-3 px-8 font-bold shadow-lg hover:shadow-md"
+                >
+                    Update
+                </button>
+                <button
+                    onClick={() => setActiveComponent('Delete')}
+                    className="mt-5 rounded-full justify-end bg-blue-600 hover:bg-blue-700 py-3 px-8 font-bold shadow-lg hover:shadow-md"
+                >
+                    Delete
+                </button>
             </div>
             {activeComponent === 'GetAll' && <GetAll />}
             {activeComponent === 'GetById' && <GetById />}
             {activeComponent === 'PostForm' && <PostForm />}
+            {activeComponent === 'Update' && <Update />}
+            {activeComponent === 'Delete' && <Delete />}
         </>
     );
 }
