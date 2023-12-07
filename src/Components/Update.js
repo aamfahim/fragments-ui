1f5e1afdcac8356bf47e8d39ecfeaf80d5db1c5d
aamfahim <97575047+aamfahim@users.noreply.github.com>
2023-12-07 14:34:49 -0500

    added image update logic

--- src/Components/Update.js
+++ src/Components/Update.js
@@ -1,17 +1,50 @@
+import React, { useState } from 'react';
 import { updateUserFragment } from '../api';
 import { getUser } from '../auth';
-import React from 'react';
 import { useForm, Controller } from 'react-hook-form';
 
 export default function Update() {
+    const [fileMismatch, setfileMismatch] = useState("");
     const { control, handleSubmit, watch } = useForm();
-    const selectedType = watch("type");
+    const selectedType = watch("type", "text/plain"); // Default to "text/plain"
 
     const onSubmit = async (data) => {
         const user = await getUser();
-        const res = await updateUserFragment(user, data.id, data.type, data.input);
-        console.log(res);
-    }
+        let typeMatch = true;
+
+        console.log(data);
+        if (selectedType.startsWith('image/')) {
+            const file = data.inputFile[0]; // The actual file object should be here now
+            console.log("file is", file);
+
+            if (file) {
+                console.log("inside file");
+                const fileExtension = file.name.split('.').pop().toLowerCase();
+                let selectedTypeExtension = selectedType.split('/').pop().toLowerCase();
+                console.log("fileExtension is", fileExtension);
+                console.log("selectedTypeExtension is", selectedTypeExtension);
+                if (selectedTypeExtension == "jpeg") {
+                    selectedTypeExtension = "jpg";
+                }
+
+                if (!["png", "jpg", "gif", "webp"].includes(fileExtension) || selectedTypeExtension !== fileExtension) {
+                    setfileMismatch("The file type and the selected type do not match.");
+                    typeMatch = false;
+                } else {
+                    setfileMismatch("");
+                    console.log("data is", data);
+
+                    const res = await updateUserFragment(user, data.id, data.type, file);
+                    console.log(res);    
+                }
+            }
+        } else {
+            const res = await updateUserFragment(user, data.id, data.type, data.input);
+            console.log(res);
+            console.log(res);
+        }
+
+    };
 
     return (
         <form onSubmit={handleSubmit(onSubmit)}>
@@ -45,11 +78,21 @@ export default function Update() {
                 />
                 {selectedType && selectedType.startsWith('image') ? (
                     <Controller
-                        name="input"
-                        control={control}
-                        defaultValue=""
-                        render={({ field }) => <input {...field} type="file" className="p-5 rounded-xl mt-2" />}
-                    />
+                    name="inputFile"
+                    control={control}
+                    defaultValue=""
+                    render={({ field: { onChange, name, ref } }) => (
+                        <input
+                            type="file"
+                            name={name}
+                            ref={ref}
+                            onChange={(e) => {
+                                onChange(e.target.files); // Update the form state
+                            }}
+                            className="p-5 rounded-xl mt-2"
+                        />
+                    )}
+                />
                 ) : (
                     <Controller
                         name="input"
@@ -58,6 +101,7 @@ export default function Update() {
                         render={({ field }) => <textarea {...field} className="p-5 w-full h-44 rounded-xl mt-2" />}
                     />
                 )}
+                {fileMismatch && <p className="text-red-500">{fileMismatch}</p>}
                 {/* update Button */}
                 <button type="submit" className='mt-5 rounded-full justify-end bg-emerald-400 hover:bg-emerald-600 py-3 px-8 font-bold shadow-lg hover:shadow-md'>Update</button>
             </div>
