b79e770b97616857fed203fffa22152a42762419
aamfahim <97575047+aamfahim@users.noreply.github.com>
2023-12-07 10:32:46 -0500

    added image posting support

--- src/Components/PostForm.js
+++ src/Components/PostForm.js
@@ -1,40 +1,54 @@
+import React, { useState } from 'react';
 import { postUserFragment } from '../api';
 import { getUser } from '../auth';
-import React from 'react';
 import { useForm, Controller } from 'react-hook-form';
 
 export default function PostForm() {
+    const [fileMismatch, setfileMismatch] = useState("");
     const { control, handleSubmit, watch } = useForm();
-    const selectedType = watch("type");
+    const selectedType = watch("type", "text/plain"); // Default to "text/plain"
 
-    // const onSubmit = async (data) => {
-    //     const user = await getUser();
-    //     const res = await postUserFragment(user, data);
-    //     console.log(data);
-    //     console.log(res);
-    // };
 
     const onSubmit = async (data) => {
         const user = await getUser();
+        let typeMatch = true;
 
-        let formData = data;
-
+        console.log(data);
         if (selectedType.startsWith('image/')) {
-            const file = data.inputFile[0]; // Assuming the name of your file input is "inputFile"
+            const file = data.inputFile[0]; // The actual file object should be here now
+            console.log("file is", file);
+
             if (file) {
-                const blob = new Blob([file], { type: selectedType });
-                formData = { ...data, inputFile: blob };
+                console.log("inside file");
+                const fileExtension = file.name.split('.').pop().toLowerCase();
+                let selectedTypeExtension = selectedType.split('/').pop().toLowerCase();
+                console.log("fileExtension is", fileExtension);
+                console.log("selectedTypeExtension is", selectedTypeExtension);
+                if (selectedTypeExtension == "jpeg") {
+                    selectedTypeExtension = "jpg";
+                }
+
+                if (!["png", "jpg", "gif", "webp"].includes(fileExtension) || selectedTypeExtension !== fileExtension) {
+                    setfileMismatch("The file type and the selected type do not match.");
+                    typeMatch = false;
+                } else {
+                    setfileMismatch("");
+                    console.log("data is", data);
+
+                    const res = await postUserFragment(user, data.type, file);
+                    console.log(res);    
+                }
             }
+        } else {
+            const res = await postUserFragment(user, data.type, data.input);
+            console.log(res);
         }
 
-        const res = await postUserFragment(user, formData);
-        console.log(formData);
-        console.log(res);
     };
 
 
     return (
-        <form onSubmit={handleSubmit(onSubmit)}>
+        <form onSubmit={handleSubmit(onSubmit)} >
             <div>
                 <Controller
                     name="type"
@@ -58,7 +72,17 @@ export default function PostForm() {
                         name="inputFile"
                         control={control}
                         defaultValue=""
-                        render={({ field }) => <input {...field} type="file" className="p-5 rounded-xl mt-2" />}
+                        render={({ field: { onChange, name, ref } }) => (
+                            <input
+                                type="file"
+                                name={name}
+                                ref={ref}
+                                onChange={(e) => {
+                                    onChange(e.target.files); // Update the form state
+                                }}
+                                className="p-5 rounded-xl mt-2"
+                            />
+                        )}
                     />
                 ) : (
                     <Controller
@@ -68,6 +92,7 @@ export default function PostForm() {
                         render={({ field }) => <textarea {...field} className="p-5 w-full h-44 rounded-xl mt-2" />}
                     />
                 )}
+                {fileMismatch && <p className="text-red-500">{fileMismatch}</p>}
                 {/* Post Button */}
                 <button type="submit" className='mt-5 rounded-full justify-end bg-emerald-400 hover:bg-emerald-600 py-3 px-8 font-bold shadow-lg hover:shadow-md'>Post</button>
             </div>
